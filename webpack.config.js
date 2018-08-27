59dc6338e00074ddeb7dcfbb91ada0ec42a70f25
vlad1918 <dima.vlad@gmail.com>
2018-08-27 14:29:34 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//npm install --save-dev webpack webpack-dev-server webpack-cli babel-core babel-loader babel-preset-env babel-preset-stage-2 babel-preset-react react-hot-loader
+//npm install --save react react-dom
+// "start": "webpack-dev-server --config ./webpack.config.js --mode development",
+// "babel": {
+//   "presets": [
+//     "env",
+//     "react",
+//     "stage-2"
+//   ]
+// },
+
+const webpack = require('webpack');
+
+module.exports = {
+	entry : './src/main/resources/src/index.js',
+	module : {
+		rules : [ {
+			test : /\.(js|jsx)$/,
+			exclude : /node_modules/,
+			use : [ 'babel-loader' ]
+		} ]
+	},
+	resolve : {
+		extensions : [ '*', '.js', '.jsx' ]
+	},
+	output : {
+		path : __dirname + '/src/main/resources/static',
+		publicPath : '/',
+		filename : 'bundle.js'
+	},
+	plugins : [ new webpack.HotModuleReplacementPlugin() ],
+	devServer : {
+		contentBase : './src/main/resources/static',
+		hot : true
+	}
+};
